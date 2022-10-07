850a62eb6082d8467c7b0bc13b8c3b724a7e8346
BoboKon <115220639+BoboKon@users.noreply.github.com>
2022-10-07 13:14:57 +0200

    doplnil som text

--- src/Main2.java
+++ src/Main2.java
@@ -1,5 +1,6 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello World in Main2");
+        System.out.println("Hello World in Main3 napisane v Githube")
     }
 }
